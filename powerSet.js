26ce1b07e227e2d46dd13e451120ce13b50e1cde
Biswajit Sen <biswajitsen@Biswajits-MacBook-Pro.local>
2023-05-13 22:48:59 +0530

    Wrote a function to calculate power set of n numbers, n <= 23.

---
+++ powerSet.js
@@ -0,0 +1,11 @@
+const powerSet = (set) => {
+  if (set.length === 0) {
+    return [[]];
+  }
+  const powerSetOfRest = powerSet(set.slice(1));
+  return [...powerSetOfRest,
+  ...powerSetOfRest.map((subSet) => [...subSet, set[0]])
+  ];
+}
+
+console.log(powerSet([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]).length);
